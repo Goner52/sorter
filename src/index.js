e081e251179a6780cbeea512684e316c1800311a
goner52 <sendas52@gmail.com>
2018-09-24 22:17:15 +0300

    ff

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 class Sorter {
   constructor() {
     // your implementation
+      this.array = [];
+
   }
 
   add(element) {
     // your implementation
+      this.array.push(element);
   }
 
   at(index) {
     // your implementation
+      return this.array[index];
   }
 
   get length() {
     // your implementation
+      return this.array.length;
   }
 
   toArray() {
     // your implementation
+      return this.array;
   }
 
   sort(indices) {
     // your implementation
+      let subArray = indices.sort().map(index => this.array[index]).sort(this.compareFunction || ((left, right) => left - right));
+      indices.forEach((index, iterator) => this.array[index] = subArray[iterator]);
   }
 
   setComparator(compareFunction) {
     // your implementation
+      this.compareFunction = compareFunction;
   }
 }
 
